0bf86cb7db8c6e9dc8340c1bfddffeca57061d08
sun <sun@codebrick.co>
2022-08-01 20:39:20 +0900

    Fix Road

--- Assets/Scripts/Road.cs
+++ Assets/Scripts/Road.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Road : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<GameObject> roads;
+    [SerializeField] private List<GameObject> obstacles;
+    [SerializeField] private GameObject rightEnd;
+    [SerializeField] private GameObject leftEnd;
+    
+    private GameObject _road;
+
+    private const float RoadInterval = 1.03f;
+
+    private void Start()
     {
-        
+            
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
